6a675660b06098798182b1b91f7fc656b9c3c72c
Zugeyifang <luhongxuan2409@outlook.com>
2025-08-04 17:50:57 +0800

    Site updated: 2025-08-04 17:50:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/03/首次尝试/","2025/08/03/tst/"];function toRandomPost(){
+var posts=["2025/08/03/tst/","2025/08/03/首次尝试/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
