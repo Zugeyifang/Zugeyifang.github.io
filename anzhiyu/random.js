946587394a1e24aa82a3b40542dce258216bb198
Zugeyifang <luhongxuan2409@outlook.com>
2025-08-04 19:04:55 +0800

    Site updated: 2025-08-04 19:04:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/03/tst/","2025/08/03/首次尝试/"];function toRandomPost(){
+var posts=["2025/08/03/tst/","2025/08/04/起点、起步/","2025/08/03/首次尝试/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
